0de5e7ee5634652bc3b7369fba914056974b1e67
Aaron Klinker <apklinker@sourceallies.com>
2021-04-08 14:06:13 -0500

    fix: Use HackerRank Icon for chat messages

--- src/bot/constants.ts
+++ src/bot/constants.ts
@@ -1,2 +1,3 @@
 export const BOT_USERNAME = 'HackerRack Bot';
-export const BOT_ICON_URL = '';
+export const BOT_ICON_URL =
+  'https://avatars.slack-edge.com/2020-01-09/888294288899_3b3b0325f633851e130c_192.png';
