e67d655eab0a546b58ae883b77d0bd755c9dff0f
Aaron Klinker <apklinker@sourceallies.com>
2021-05-20 12:21:44 -0500

    chore: use `process.env.SPREADSHEET_ID` instead of a hardcoded string

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,8 @@
 import { GoogleSpreadsheet, GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
 
 export const database = {
-  qualFileId: '1ChOEjl5l_Uh5dTd_fRjGJt8z7bFNNOwPlzgRnhOLgTY',
-
   async open(): Promise<GoogleSpreadsheet> {
-    const document = new GoogleSpreadsheet(this.qualFileId);
+    const document = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
     await document.useServiceAccountAuth({
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL ?? '',
       private_key: process.env.GOOGLE_PRIVATE_KEY ?? '',
