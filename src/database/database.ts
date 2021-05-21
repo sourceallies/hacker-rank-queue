180ee5334926f1cac3f3bebfdde8429ee3e07aa1
Aaron Klinker <apklinker@sourceallies.com>
2021-05-21 12:50:29 -0500

    chore: cache database instance for 60s
    This will speed up requests because each repo won't have to open and re-authenticate access to the spreadsheet

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,18 +1,26 @@
 import { GoogleSpreadsheet, GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
 
 export const database = {
+  document: undefined as GoogleSpreadsheet | undefined,
+
   async open(): Promise<GoogleSpreadsheet> {
+    console.info('Opening spreadsheet...');
     const document = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
     await document.useServiceAccountAuth({
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL ?? '',
       private_key: process.env.GOOGLE_PRIVATE_KEY ?? '',
     });
     await document.loadInfo();
+    this.document = this.document;
+    setTimeout(() => {
+      console.info('Cleared cached spreadsheet after 60s');
+      this.document = undefined;
+    }, 60 * 1000);
     return document;
   },
 
   async openSheet(sheetTitle: string, columns: string[]): Promise<GoogleSpreadsheetWorksheet> {
-    const doc = await this.open();
+    const doc = this.document ?? (await this.open());
     const sheet = doc.sheetsByTitle[sheetTitle];
     if (sheet != null) {
       sheet.setHeaderRow(columns);
