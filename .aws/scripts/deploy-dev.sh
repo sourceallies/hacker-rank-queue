8008d7b61698cb0e5f991fe56dca3e54c48fd60f
Aaron Klinker <apklinker@sourceallies.com>
2021-07-22 08:52:09 -0500

    chore: add wait and login steps to deploy script

--- .aws/scripts/deploy-dev.sh
+++ .aws/scripts/deploy-dev.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
+echo ""
+
+CALLER_ID="$(aws sts get-caller-identity --no-cli-pager | grep 729161019481)"
+if [[ "$CALLER_ID" == "" ]]; then
+    echo -e "\x1b[96m\x1b[1mSign into AWS dev\x1b[0m"
+    if [[ "$(which dev | grep 'dev: aliased to pushd')" != "" ]]; then
+        dev
+    else
+        echo "'dev' alias not found, sign in manually"
+        exit 1
+    fi
+fi
 
 # Build and push the image
 pushd ..
@@ -14,5 +26,15 @@ function getOutput {
     echo "$(aws cloudformation describe-stacks --stack-name HackerRankQueueStack --query "Stacks[0].Outputs[?OutputKey=='$key'].OutputValue" --output text)"
 }
 CLUSTER_NAME="$(getOutput ClusterName)"
-SEERVICE_NAME="$(getOutput ServiceName)"
-aws ecs update-service --cluster "$CLUSTER_NAME" --service "$SEERVICE_NAME" --force-new-deployment --no-cli-pager
+SERVICE_NAME="$(getOutput ServiceName)"
+aws ecs update-service --cluster "$CLUSTER_NAME" --service "$SERVICE_NAME" --force-new-deployment --no-cli-pager
+
+echo ""
+echo "Waiting for app to update..."
+
+aws ecs wait services-stable \
+    --cluster "$CLUSTER_NAME" \
+    --services "$SERVICE_NAME"
+
+echo -e "\x1b[92m\x1b[1mECS Task started!\x1b[0m"
+echo ""
