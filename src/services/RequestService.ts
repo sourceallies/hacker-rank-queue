9ee947ce4e51f35beb8f6652686c9e8d886e919a
Aaron Klinker <apklinker@sourceallies.com>
2021-08-03 17:04:55 -0500

    Refactored stubbed definition for declining the request

--- src/services/RequestService.ts
+++ src/services/RequestService.ts
@@ -1,33 +1,14 @@
+import { ActiveReview } from '@/database/models/ActiveReview';
 import { WebClient } from '@/slackTypes';
 
 /**
  * Notify users their time is up and request the next person
  */
-export async function expireRequest(
+export async function declineRequest(
   client: WebClient,
-  review: unknown,
-  expiredUserId: string,
+  activeReview: Readonly<ActiveReview>,
+  declinedUserId: string,
+  expiration = false,
 ): Promise<void> {
-  // Notify expired user
-  await notifyExpiredUser(client, expiredUserId);
-
-  // Update review - mark expiredUserId as requested
-
-  // Find next user
-  const nextUserId = 'some-user';
-
-  // Notify next user
-  await notifiyNextUser(client, nextUserId);
-
-  // Update review with nextUserId as requested
-
-  // Save review
-}
-
-async function notifyExpiredUser(client: WebClient, userId: string): Promise<void> {
-  throw Error('Not implmented');
-}
-
-async function notifiyNextUser(client: WebClient, review: string): Promise<void> {
-  throw Error('Not implmented');
+  throw Error('Not implemented: RequestService.declineRequest');
 }
